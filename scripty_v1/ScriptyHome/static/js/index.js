8b67f70224900f9fc62ffc94878cfde22b8d969b
Vedant Upadhyay <vedantupadhyaya42@gmail.com>
2021-08-07 18:01:35 +0530

    Fixed Dictionary

--- scripty_v1/ScriptyHome/static/js/index.js
+++ scripty_v1/ScriptyHome/static/js/index.js
@@ -11,185 +11,254 @@ let editor;
 //         } );
 
 $().ready(function () {
+  document.ondblclick = function () {
+    var sel =
+      (document.selection && document.selection.createRange().text) ||
+      (window.getSelection && window.getSelection().toString());
+    document.getElementById("wordInput").value = sel;
+  };
 
-    document.ondblclick = function () {
-        var sel = (document.selection && document.selection.createRange().text) ||
-            (window.getSelection && window.getSelection().toString());
-        document.getElementById("wordInput").value = sel;
-    };
-
-    $("#synbtn").submit(function (e) {
-        let word = document.getElementById("wordInput").value;
-        var form_elements = document.getElementById('synform').elements;
-        var selectedRadio = form_elements['optionsRadios'].value;
-        e.preventDefault();
-        switch (selectedRadio) {
-            case ("synonym"):
-                console.log(("synonym"))
-                $.ajax(
-                    {
-                        type: "POST",
-                        url: "/gosynonym",
-                        data: {
-                            word: word
-                        },
-                        success: function (data) {
-                            console.log(data);
-                            document.getElementById("wordOutput").innerHTML = data;
-                        }
-                    });
-                break;
-            case ("meaning"):
-                $.ajax(
-                    {
-                        type: "POST",
-                        url: "/gomeaning",
-                        data: {
-                            word: word
-                        },
-                        success: function (data) {
-                            console.log(data);
-                            document.getElementById("wordOutput").innerHTML = data;
-                        }
-                    });
-                break;
-            case ("antonym"):
-                $.ajax(
-                    {
-                        type: "POST",
-                        url: "/goantonym",
-                        data: {
-                            word: word
-                        },
-                        success: function (data) {
-                            console.log(data);
-                            document.getElementById("wordOutput").innerHTML = data;
-                        }
-                    });
-                break;
-            default:
-                $.ajax(
-                    {
-                        type: "POST",
-                        url: "/gosynonym",
-                        data: {
-                            word: word
-                        },
-                        success: function (data) {
-                            console.log(data);
-                            document.getElementById("wordOutput").innerHTML = data;
-                        }
-                    });
-                break;
-        }
-        // $.ajax({
-        //     url: '/getDict',
-        //     type: 'post',
-        //     data: {
-        //         'word': word,
-        //         'find':selectedRadio,
-        //         csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
-        //     },
-
-        //     success: function(data) {
-        //         console.log(data);
-        //         document.getElementById("textInput").innerHTML = data;
-        //     },
-        //     error: function(data) {
-        //         console.log('error');
-        //     }
-        // });
-    });
-
-    $("#translateForm").submit(function (e) {
-        e.preventDefault();
-        let fromLanguage = $("#fromLanguage").val();
-        let toLanguage = $("#toLanguage").val();
-        //console.log(toLanguage);
-
-        let text = document.getElementById("textInput").innerHTML;
-
+  $("#synform").submit(function (e) {
+    e.preventDefault();
+    let word = document.getElementById("wordInput").value;
+    var form_elements = document.getElementById("synform").elements;
+    var selectedRadio = form_elements["optionsRadios"].value;
+    e.preventDefault();
+    switch (selectedRadio) {
+      case "synonym":
+        console.log("synonym");
         $.ajax({
-            type: "post",
-            url: "/translateText",
-            data: {
-                text: text,
-                src: fromLanguage,
-                dest: toLanguage,
-                csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
-            },
-            beforeSend: function () {
-                $('#loader').removeClass('hidden')
-            },
-            success: function (response) {
-                // console.log(response);
-                $('#loader').addClass('hidden')
-                document.getElementById("textInput").innerHTML  = response.result;
-                // editor.setData(response.result);
-            },
-            error: function (response) {
-                $('#loader').addClass('hidden')
-                console.log(response);
-            },
-            complete: function () {
-                $('#loader').addClass('hidden')
-            },
-        });
-
-    });
-
-    $("#readPdfImage").submit(function (e) {
-        e.preventDefault();
-
-        //getting the file..
-        var form = new FormData($(this).get(0));
-
-        $.ajax({
-            url: 'readPdfImage',
-            type: 'POST',
-            data: form,
-            cache: false,
-            processData: false,
-            contentType: false,
-            success: function (data) {
-                console.log(data);
-                document.getElementById("textInput").innerHTML = data;
-            },
-            error: function (data) {
-                console.log('error');
+          type: "POST",
+          url: "/gosynonym",
+          data: {
+            csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
+            word: word,
+          },
+          success: function (data) {
+            console.log(data);
+            if (data.result.length == 0) {
+              $("#wordOutput").html("No antonyms found");
+            } else {
+              $("#wordOutput").html(data.result.slice(0, 3).toString(","));
             }
+          },
+          beforeSend: function () {
+            $("#loader").removeClass("hidden");
+          },
+
+          error: function (response) {
+            $("#loader").addClass("hidden");
+            console.log(response);
+          },
+          complete: function () {
+            $("#loader").addClass("hidden");
+          },
         });
-    });
-
-    $('#checkerForm').submit(function (e) {
-        $("#ocrFileInput").modal('hide');
-        e.preventDefault();
-        //close modal ocrFileInput
-
-        // let data = editor.getData();
-        let data = document.getElementById("textInput").innerHTML;
+        break;
+      case "meaning":
         $.ajax({
-            type: "post",
-            url: "grammarCheck",
-            data: {
-                text: data,
-                csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
-            },
-            beforeSend: function () {
-                $('#loader').removeClass('hidden')
-            },
-            success: function (response) {
-                console.log(response.result, typeof (response));
-                console.log(new DOMParser().parseFromString(response.result.result_colored, "text/html"));
-                // editor.setData(response.result.result_colored);
-                document.getElementById("textInput").innerHTML = response.result.result_colored;
-            },
-            error: function (response) {
-                console.log(response);
-            },
-            complete: function () {
-                $('#loader').addClass('hidden')
-            },
+          type: "POST",
+          url: "/gomeaning",
+          data: {
+            csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
+            word: word,
+          },
+          success: function (data) {
+            console.log(data);
+            if (data.result.Noun.length == 0) {
+              $("#wordOutput").html("No meaning found");
+            } else {
+              $("#wordOutput").html(`Noun - ${data.result.Noun[0]}`);
+              if (data.result.Verb !== undefined) {
+                $("#wordOutput").append(`\nVerb - ${data.result.Verb[0]}`);
+              }
+            }
+          },
+          beforeSend: function () {
+            $("#loader").removeClass("hidden");
+          },
+
+          error: function (response) {
+            $("#loader").addClass("hidden");
+            console.log(response);
+          },
+          complete: function () {
+            $("#loader").addClass("hidden");
+          },
         });
-    })
+        break;
+      case "antonym":
+        $.ajax({
+          type: "POST",
+          url: "/goantonym",
+          data: {
+            csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
+            word: word,
+          },
+          success: function (data) {
+            console.log(data);
+            if (data.result.length == 0) {
+              $("#wordOutput").html("No antonyms found");
+            } else {
+              $("#wordOutput").html(data.result.slice(0, 3).toString(","));
+            }
+          },
+          beforeSend: function () {
+            $("#loader").removeClass("hidden");
+          },
+
+          error: function (response) {
+            $("#loader").addClass("hidden");
+            console.log(response);
+          },
+          complete: function () {
+            $("#loader").addClass("hidden");
+          },
+        });
+        break;
+      default:
+        $.ajax({
+          type: "POST",
+          url: "/gosynonym",
+          data: {
+            csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
+            word: word,
+          },
+          success: function (data) {
+            console.log(data);
+            if (data.result.length == 0) {
+              $("#wordOutput").html("No Synonyms found");
+            } else {
+              $("#wordOutput").html(data.result.slice(0, 3).toString(","));
+            }
+          },
+          beforeSend: function () {
+            $("#loader").removeClass("hidden");
+          },
+
+          error: function (response) {
+            $("#loader").addClass("hidden");
+            console.log(response);
+          },
+          complete: function () {
+            $("#loader").addClass("hidden");
+          },
+        });
+        break;
+    }
+    // $.ajax({
+    //     url: '/getDict',
+    //     type: 'post',
+    //     data: {
+    //         'word': word,
+    //         'find':selectedRadio,
+    //         csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
+    //     },
+
+    //     success: function(data) {
+    //         console.log(data);
+    //         document.getElementById("textInput").innerHTML = data;
+    //     },
+    //     error: function(data) {
+    //         console.log('error');
+    //     }
+    // });
+  });
+
+  $("#translateForm").submit(function (e) {
+    e.preventDefault();
+    let fromLanguage = $("#fromLanguage").val();
+    let toLanguage = $("#toLanguage").val();
+    //console.log(toLanguage);
+
+    let text = document.getElementById("textInput").innerHTML;
+
+    $.ajax({
+      type: "post",
+      url: "/translateText",
+      data: {
+        text: text,
+        src: fromLanguage,
+        dest: toLanguage,
+        csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val(),
+      },
+      beforeSend: function () {
+        $("#loader").removeClass("hidden");
+      },
+      success: function (response) {
+        // console.log(response);
+        $("#loader").addClass("hidden");
+        document.getElementById("textInput").innerHTML = response.result;
+        // editor.setData(response.result);
+      },
+      error: function (response) {
+        $("#loader").addClass("hidden");
+        console.log(response);
+      },
+      complete: function () {
+        $("#loader").addClass("hidden");
+      },
+    });
+  });
+
+  $("#readPdfImage").submit(function (e) {
+    e.preventDefault();
+
+    //getting the file..
+    var form = new FormData($(this).get(0));
+
+    $.ajax({
+      url: "readPdfImage",
+      type: "POST",
+      data: form,
+      cache: false,
+      processData: false,
+      contentType: false,
+      success: function (data) {
+        console.log(data);
+        document.getElementById("textInput").innerHTML = data;
+      },
+      error: function (data) {
+        console.log("error");
+      },
+    });
+  });
+
+  $("#checkerForm").submit(function (e) {
+    $("#ocrFileInput").modal("hide");
+    e.preventDefault();
+    //close modal ocrFileInput
+
+    // let data = editor.getData();
+    let data = document.getElementById("textInput").innerHTML;
+    $.ajax({
+      type: "post",
+      url: "grammarCheck",
+      data: {
+        text: data,
+        csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val(),
+      },
+      beforeSend: function () {
+        $("#loader").removeClass("hidden");
+      },
+      success: function (response) {
+        console.log(response.result, typeof response);
+        console.log(
+          new DOMParser().parseFromString(
+            response.result.result_colored,
+            "text/html"
+          )
+        );
+        // editor.setData(response.result.result_colored);
+        document.getElementById("textInput").innerHTML =
+          response.result.result_colored;
+      },
+      error: function (response) {
+        console.log(response);
+      },
+      complete: function () {
+        $("#loader").addClass("hidden");
+      },
+    });
+  });
 });
