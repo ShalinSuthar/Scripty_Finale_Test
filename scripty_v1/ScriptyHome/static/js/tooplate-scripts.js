9103b7fbe0337bed30bcb1a0d2ddc243bdf434a8
Shalin Suthar <48246090+ShalinSuthar@users.noreply.github.com>
2021-08-10 18:56:42 +0530

    UI changes

--- scripty_v1/ScriptyHome/static/js/tooplate-scripts.js
+++ scripty_v1/ScriptyHome/static/js/tooplate-scripts.js
@@ -31,11 +31,7 @@ function drawLineChart() {
           "May",
           "June",
           "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December"
+          "August"
         ],
         datasets: [
           {
@@ -75,7 +71,7 @@ function drawBarChart() {
             },
             scaleLabel: {
               display: true,
-              labelString: "Hits"
+              labelString: "Errors"
             }
           }
         ]
@@ -88,18 +84,23 @@ function drawBarChart() {
     configBar = {
       type: "bar",
       data: {
-        labels: ["welcome", "hello", "student", "color", "master", "are"],
+        labels: ["welcome", "hello", "student", "color", "master", "are","great","elephant","nueron","successful","always"],
         datasets: [
           {
             label: "No of errors",
-            data: [1112, 909, 683, 255, 222, 213],
+            data: [1340,909,853,829,739,644,123,119,88,81,25],
             backgroundColor: [
               "rgba(255, 99, 132, 0.2)",
               "rgba(54, 162, 235, 0.2)",
               "rgba(255, 206, 86, 0.2)",
               "rgba(75, 192, 192, 0.2)",
               "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)"
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(25, 59, 64, 1)",
+              "rgba(155, 189, 133, 1)",
+              "rgba(255, 255, 64, 1)",
+              "rgba(155, 159, 144, 1)",
+              "rgba(55, 159, 164, 1)"
             ],
             borderColor: [
               "rgba(255,99,132,1)",
@@ -107,7 +108,12 @@ function drawBarChart() {
               "rgba(255, 206, 86, 1)",
               "rgba(75, 192, 192, 1)",
               "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)"
+              "rgba(255, 159, 64, 1)",
+              "rgba(25, 59, 64, 1)",
+              "rgba(155, 189, 133, 1)",
+              "rgba(255, 255, 64, 1)",
+              "rgba(155, 159, 144, 1)",
+              "rgba(55, 159, 164, 1)"
             ],
             borderWidth: 1
           }
